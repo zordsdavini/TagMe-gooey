3c59823e0e42ab218c62cd286a78d7e347d28493
Arnas Udovicius <arnas.u@estina.lt>
2020-02-08 15:33:42 +0200

    UI for remove tag from files/directory, clean directory

--- TagMeGooey/gooey_ui.py
+++ TagMeGooey/gooey_ui.py
@@ -53,6 +53,45 @@ def main():
                                       choices=Tag.TAG_LIST,
                                       nargs="*"
                                       )
+    remove_tag_from_file = subs.add_parser('remove_tag_from_file',
+                                           help='remove tags from selected files'
+                                           )
+    remove_tag_from_file.add_argument('files',
+                                      help="select files to process",
+                                      widget="MultiFileChooser"
+                                      )
+    remove_tag_from_file.add_argument('tags',
+                                      help="select tags",
+                                      widget='Listbox',
+                                      choices=Tag.TAG_LIST,
+                                      nargs="*"
+                                      )
+
+    remove_tag_from_directory = subs.add_parser('remove_tag_from_directory',
+                                                help='add tags to selected directory recursevly'
+                                                )
+    remove_tag_from_directory.add_argument('directory',
+                                           help="select directory to process",
+                                           widget="DirChooser"
+                                           )
+    remove_tag_from_directory.add_argument('tags',
+                                           help="select tags",
+                                           widget='Listbox',
+                                           choices=Tag.TAG_LIST,
+                                           nargs="*"
+                                           )
+
+    clean_directory = subs.add_parser('clean_directory',
+                                      help='remove files from source directory what exists in destination directory'
+                                      )
+    clean_directory.add_argument('source_directory',
+                                 help="select directory to process",
+                                 widget="DirChooser"
+                                 )
+    clean_directory.add_argument('directory',
+                                 help="select directory to check",
+                                 widget="DirChooser"
+                                 )
     args = parser.parse_args()
 
     if args.command == 'add_tag_to_file':
@@ -62,3 +101,21 @@ def main():
     elif args.command == 'add_tag_to_directory':
         print('Adding tag to directory...')
         command_manager.process_add_tag_to_directory(args.tags, args.directory)
+
+    elif args.command == 'remove_tag_from_file':
+        print('Removing tag to file...')
+        command_manager.process_remove_tag_from_file(
+                args.tags,
+                args.files.split(':'))
+
+    elif args.command == 'remove_tag_from_directory':
+        print('Removing tag to directory...')
+        command_manager.process_remove_tag_from_directory(
+                args.tags,
+                args.directory)
+
+    elif args.command == 'clean_directory':
+        print('Cleaning directory...')
+        command_manager.process_clean_directory(
+                args.source_directory,
+                args.directory)
